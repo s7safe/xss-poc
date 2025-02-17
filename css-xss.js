9febba7c9cc8ab9a61aa0fefccaf18fcf3e906e4
R.M6 <54035968+s7safe@users.noreply.github.com>
2025-02-17 14:08:23 +0800

    Update css-xss.js

--- css-xss.js
+++ css-xss.js
@@ -1,6 +1,9 @@
-#js_single_page_container{
-    display: none;
+#js_single_page_container {
+    display: none;
 }
-.login_page_standalone:before{
-    content:"xss-poc";
+
+.login_page_standalone:before {
+    content: "xss-poc";
+    font-size: 24px; /* 调整字体大小，可根据需要修改值 */
+    color: red; /* 设置字体颜色为红色 */
 }
